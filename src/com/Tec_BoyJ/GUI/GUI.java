b718f54e76f90759bb1963d3578e4b25119b5c0e
tecboyj <jc515081@gmail.com>
2022-12-18 21:20:06 -0500

    Shape of Fractions

--- src/com/Tec_BoyJ/GUI/GUI.java
+++ src/com/Tec_BoyJ/GUI/GUI.java
@@ -17,15 +17,16 @@ public class GUI {
     JTextField textField1 = new JTextField();
     JTextField textField2 = new JTextField();
 
-    FractionButton button = new FractionButton("GO!", 150);
-    FractionButton b23 = new FractionButton("1/23", 200);
-    FractionButton b365 = new FractionButton("1/365", 250);
-    FractionButton b47 = new FractionButton("1/47", 300);
-    FractionButton b3 = new FractionButton("1/3", 350);
-    FractionButton b2d49 = new FractionButton("2/49", 400);
-    FractionButton b100d49 = new FractionButton("100/49", 450);
-    FractionButton b3d7 = new FractionButton("3/7", 500);
-    FractionButton b56d43 = new FractionButton("56/43", 550);
+    JButton button = new JButton("GO!");
+
+    FractionButton b23 = new FractionButton("1/23", 200, 1, 23);
+    FractionButton b365 = new FractionButton("1/365", 250, 1, 365);
+    FractionButton b47 = new FractionButton("1/47", 300, 1, 47);
+    FractionButton b3 = new FractionButton("1/3", 350, 1, 3);
+    FractionButton b2d49 = new FractionButton("2/49", 400, 2, 49);
+    FractionButton b100d49 = new FractionButton("100/49", 450, 100, 49);
+    FractionButton b3d7 = new FractionButton("3/7", 500, 3, 7);
+    FractionButton b56d43 = new FractionButton("56/43", 550, 56, 43);
 
     JLabel labelGUI = new JLabel("GUI Interface");
     JButton GUI = new JButton("Python");
@@ -48,6 +49,11 @@ public class GUI {
         textField2.setFont(font);
         textField2.setHorizontalAlignment(SwingConstants.CENTER);
         panel.add(textField2);
+
+        button.setBounds(380, 150, 240, 40);
+        button.setFont(font);
+        button.addActionListener(actionListener);
+        panel.add(button);
 
         labelGUI.setBounds(90, 125, 200, 40);
         labelGUI.setFont(font);
@@ -92,29 +98,28 @@ public class GUI {
             } else if (e.getSource() == button) {
                 int x = Integer.parseInt(textField1.getText());
                 int y = Integer.parseInt(textField2.getText());
-                multiThreading(x, y);
-            } else if (e.getSource() == b23) multiThreading(1, 23);
-            else if (e.getSource() == b365) multiThreading(1, 365);
-            else if (e.getSource() == b47) multiThreading(1, 47);
-            else if (e.getSource() == b3) multiThreading(1, 3);
-            else if (e.getSource() == b2d49) multiThreading(2, 49);
-            else if (e.getSource() == b100d49) multiThreading(100, 49);
-            else if (e.getSource() == b3d7) multiThreading(3, 7);
-            else if (e.getSource() == b56d43) multiThreading(56, 43);
+                new MultiThreading(x, y);
+            }
         }
     };
 
-    public static void multiThreading(int x, int y) {
-        new MultiThreading(x, y).start();
-    }
+    public class FractionButton extends JButton implements ActionListener {
+        int num;
+        int den;
+        public FractionButton(String buttonName, int yValue, int num, int den) {
+            this.num = num;
+            this.den = den;
 
-    public class FractionButton extends JButton {
-        public FractionButton(String buttonName, int yValue) {
             this.setText(buttonName);
-            this.addActionListener(actionListener);
+            this.addActionListener(this);
             this.setBounds(380, yValue, 240, 40);
             this.setFont(new Font("Arial", Font.PLAIN, 24));
             panel.add(this);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new MultiThreading(num, den).start();
         }
     }
 }
